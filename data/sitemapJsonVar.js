e4f9b557ce6d376ac12f85648b945fb447489e3b
inonehp <inonehp@gmail.com>
2024-01-30 19:57:07 +0200

    update

--- data/sitemapJsonVar.js
+++ data/sitemapJsonVar.js
@@ -721,13 +721,6 @@ var sitemapJsonVar = [
         "time": "id"
     },
     {
-        "text": "Projects >  google programmable search > site list\n\nsite list",
-        "url": "\/projects\/49-google-programmable-search\/site-list.html",
-        "tag": " #empty",
-        "id": "2aed453afdad7d6f6c2019821c258dba",
-        "time": "id"
-    },
-    {
         "text": "Projects >  google trends > .\n\nGoogle trends",
         "url": "\/projects\/46-google-trends\/index.html",
         "tag": " #Google #trends #info #HTML #embed",
