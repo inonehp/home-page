3b01dc440b21a6ce964caaabc1597625276e760b
inonehp <inonehp@gmail.com>
2024-03-12 03:28:27 +0200

    update

--- data/sitemapJsonVar.js
+++ data/sitemapJsonVar.js
@@ -364,7 +364,7 @@ var sitemapJsonVar = [
         "time": "id"
     },
     {
-        "text": "Install\n\n\u00b7 Install website as app (PWA). ",
+        "text": "Install\n\nInstall website as app (PWA). The site or page will look and run like an app and will be among other installed programs. ",
         "url": "\/install.html",
         "tag": " #install #install_website #PWA #WebApp #app #page #main",
         "id": "3036dd8f54ff32dc48bf6258269eb20e",
