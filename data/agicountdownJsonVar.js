b61680a5f775b243585895f42f38cb8ffb056904
inonehp <inonehp@gmail.com>
2024-03-16 17:43:29 +0200

    update

--- data/agicountdownJsonVar.js
+++ data/agicountdownJsonVar.js
@@ -1,6 +1,33 @@
 var agicountdownJsonVar = 
 [
     {
+        "id": "4",
+        "text": "ChatGPT\r\n\r\n\"Name a possible year of AGI creation\"\r\n...\r\n\"year between two and five decades\"\r\n\r\n---\r\n\r\nIf we consider a conservative estimate within the range of two to five decades, then a plausible timeframe for the creation of Artificial General Intelligence (AGI) could fall somewhere between the years 2040 and 2070. \r\n\r\nPlease note that this is a speculative estimate based on current trends and the pace of advancements in AI research and technology. Numerous factors could influence the actual timeline, including breakthroughs in AI algorithms, developments in computing power, progress in understanding human cognition, and societal and ethical considerations.\r\n\r\nAs with any prediction related to AGI, it's important to approach it with caution and recognize the uncertainties inherent in such forecasts.",
+        "text2": "2055",
+        "text3": "",
+        "url": "https:\/\/chat.openai.com\/c\/603110ea-78b5-4f69-8435-1a10ced0e0f9",
+        "tag": "",
+        "time": 1710603380
+    },
+    {
+        "id": "3",
+        "text": "Copilot\r\n\r\n\"Predict the exact year when humans will invent general artificial intelligence.\"\r\n\r\nAccording to **Ben Goertzel**, a computer scientist and CEO of SingularityNET, we may be on the way to creating an artificial intelligence agent that is just as smart as humans by **2027**. He believes that humanity could build an **artificial general intelligence (AGI)** agent within the next three to eight years, which could open the door to a \"super intelligence\" in a very short space of time. The concept of **singularity**, where AI becomes just as capable as humans across several areas independent of the original training data, is within reach. While the likeliest years for building the first AGI agent are **2029 or 2030**, it could potentially happen as early as **2027**\u00b9. Keep in mind that this is a speculative estimate, and the actual timeline remains uncertain. \ud83e\udd16\ud83c\udf1f\r\n\r\nSource: Conversation with Bing, 3\/16\/2024\r\n(1) Artificial general intelligence (AGI) may arise in 2027 with artificial .... https:\/\/www.livescience.com\/technology\/artificial-intelligence\/ai-agi-singularity-in-2027-artificial-super-intelligence-sooner-than-we-think-ben-goertzel.\r\n(2) . https:\/\/bing.com\/search?q=Predict+the+exact+year+when+humans+will+invent+general+artificial+intelligence..\r\n(3) AI timelines: What do experts in artificial intelligence expect for the .... https:\/\/ourworldindata.org\/ai-timelines.\r\n(4) undefined. https:\/\/thenextweb.com\/news\/when-will-agi-arrive-tech-experts-predict-artificial-general-intelligence.\r\n(5) undefined. https:\/\/research.aimultiple.com\/artificial-general-intelligence-singularity-timing\/.\r\n(6) undefined. https:\/\/bing.com\/search?q=Predict+the+exact+year+when+humans+will+invent+general+artificial+intelligence.",
+        "text2": "2029",
+        "text3": "",
+        "url": "https:\/\/copilot.microsoft.com\/",
+        "tag": "",
+        "time": 1710602261
+    },
+    {
+        "id": "2",
+        "text": "Gemini\r\n\r\n\"Predict the exact year when humans will invent general artificial intelligence.\"\r\n\r\nI can't predict the exact year for general artificial intelligence (AGI) for a few reasons:\r\n\r\n* **The field is inherently unpredictable:**  Breakthroughs can come unexpectedly, and unforeseen challenges can arise. \r\n*  **Lack of clear definition:**  There's no universally agreed-upon definition of AGI, making it difficult to pinpoint its arrival.\r\n\r\nWhile I can't give you an exact year, I can offer some possibilities:\r\n\r\n* **Highly speculative:** Some optimists predict AGI within the next decade (before 2034). \r\n* **More realistic range:** Many experts suggest a timeframe between 2040 and 2070. \r\n* **Long-term view:**  Others believe achieving AGI could take much longer, even centuries.\r\n\r\nIt's important to remember these are just educated guesses. The true timeline for AGI remains uncertain.",
+        "text2": "2055",
+        "text3": "",
+        "url": "https:\/\/gemini.google.com\/app\/9d95f216592c07dc",
+        "tag": "",
+        "time": 1710601924
+    },
+    {
         "id": "1",
         "text": "Date Weakly General AI is Publicly Known | Metaculus",
         "text2": "2026",
