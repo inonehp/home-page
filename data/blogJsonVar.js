46cb68d5d35737a5e7ae19954d89b03936023c9e
inonehp <inonehp@gmail.com>
2024-03-20 09:19:51 +0200

    update

--- data/blogJsonVar.js
+++ data/blogJsonVar.js
@@ -667,15 +667,6 @@ var blogJsonVar =
         "time": 1689415882
     },
     {
-        "id": "100",
-        "text": "Centering a div\r\n\r\nCSS:\r\n\r\n<style>\r\n.centeringItem {\r\ndisplay: flex;\r\nflex-direction: column;\r\njustify-content: center;\r\nalign-items: safe center;\r\nalign-self: safe center;\r\nbox-sizing: border-box;\r\nmargin: 0 auto;\r\nmin-height: 100vh;\r\n}\r\n<\/style>\r\n\r\nHTML:\r\n\r\n<div class=\"centeringItem\">\r\n<div><!-- wrapper -->\r\n\r\n<h1>item<\/h1>\r\n\r\n<\/div>\r\n<\/div>",
-        "text2": "",
-        "text3": "",
-        "url": "",
-        "tag": "#i #CSS",
-        "time": 1689262345
-    },
-    {
         "id": "52",
         "text": "Start End",
         "text2": "",
