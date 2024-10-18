fd8ab8767c0b7f71dce20d157419c35bb3e2fb0a
irvirty <irvirty@gmail.com>
2024-10-18 21:58:09 +0300

    update

--- data/newsJsonVar.js
+++ data/newsJsonVar.js
@@ -3,6 +3,16 @@ var newsJsonVar =
 
 [
     {
+        "id": "2",
+        "text": "Bluesky: \"congratulations everyone, we have now passed 12 million people total on bluesky!!! \ud83e\udd8b over 1.2M new people have joined bluesky in the last two days \u2014 welcome!! \ud83d\udc99\ud83d\udd7a\ud83e\udea9\" \u2014 Bluesky",
+        "text2": "",
+        "text3": "",
+        "url": "https:\/\/bsky.app\/profile\/bsky.app\/post\/3l6sjdebqbx2q",
+        "tag": "#SocialMedia #SocialNetwork",
+        "time": 1729275139,
+        "rightFooter": ""
+    },
+    {
         "id": "1",
         "text": "Bluesky: \"\ud83d\udce2\u00a0App Version 1.92.1 is rolling out now (1\/2) This release includes multiple bugfixes and UI tweaks. Thanks everyone for your feedback on the last release!\" \u2014 Bluesky",
         "text2": "",
