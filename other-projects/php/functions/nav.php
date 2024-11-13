db0e904a529653c5af11fefbd3c42c075b7c7377
irvirty <irvirty@gmail.com>
2024-11-13 14:05:49 +0200

    update

--- other-projects/php/functions/nav.php
+++ other-projects/php/functions/nav.php
@@ -1,28 +1,48 @@
 <?PHP
 // v.1.0.0
 
+/*$navArr = array(
+//"{$confLocalPatchOffline}pages-list.$ext" => "Pages",
+"./pages/" => "Pages",
+"./projcets/" => "Projects",
+);
+echo nav($navArr, "pages", "");*/
 
-function nav($fuNavArr, $fuNavCurrentPage, $ext){
+function nav($navArr, $navCurrentPage, $ext){
 
-$fuNavMenu = "";
-foreach($fuNavArr as $v => $key){
+$navMenu = "";
 
-if (count(explode($fuNavCurrentPage, $v)) >= 2){
+$navCurrentPage2 = basename($_SERVER["SCRIPT_FILENAME"], '.php');
+if ($navCurrentPage2 == "index"){
+if (php_uname('s') == 'Windows NT'){
+$dirname = explode("\\", getcwd());
+$dirname = $dirname[count(explode('\\', getcwd()))-1];
+//$d = $_SERVER["DOCUMENT_ROOT"].'/';
+} else {
+$dirname = explode("/", getcwd());
+$dirname = $dirname[count(explode("/", getcwd()))-1];
+}
+$navCurrentPage2 = $dirname;
+}
 
-$fuNavMenu .= <<<EOF
+//if (empty($navCurrentPage)){ $navCurrentPage = $navCurrentPage2; }
+
+foreach ($navArr as $v => $key){
+if (count(explode($navCurrentPage, $v)) >= 2||count(explode($navCurrentPage2, $v)) >= 2){
+$navMenu .= <<<EOF
 <a class="countMenuItem active2 inlineBlock padding itemLinkAniActive" href="$v">$key</a>
 
 EOF;
 } else {
-$fuNavMenu .= <<<EOF
+$navMenu .= <<<EOF
 <a class="countMenuItem inlineBlock padding brand itemLinkAni" href="$v">$key</a>
 
 EOF;
 }
-
+//echo $v."<hr>";
 }
 
-return $fuNavMenu;
+return $navMenu;
 }
 
 ?>
